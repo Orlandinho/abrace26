c6f099860b547aa92230f8eea8560a361823540f
Orlandinho <horlandomagic@gmail.com>
2026-01-12 23:03:18 -0300

    Testing: extended the assertInertia method in Pest to check for resourced and paginated collections in testing

---
+++ app/Providers/TestingServiceProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Testing\TestResponse;
+use Inertia\Testing\AssertableInertia;
+
+class TestingServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        if (! $this->app->runningUnitTests()) {
+            return;
+        }
+
+        AssertableInertia::macro('hasResource', function (string $key, JsonResource $resource) {
+            $props = $this->toArray()['props'];
+
+            $compiledResource = $resource->response()->getData(true);
+
+            expect($props)
+                ->toHaveKey($key, message: "Key {$key} not passed as a props to Inertia")
+                ->and($props[$key])
+                ->toEqual($compiledResource, message: "Resource {$key} does not match");
+
+            return $this;
+        });
+
+        AssertableInertia::macro('hasPaginatedResource', function (string $key, ResourceCollection $resource) {
+            $props = $this->toArray()['props'];
+
+            $compiledResource = $resource->response()->getData(true);
+
+            expect($props)
+                ->toHaveKeys(['data', 'link', 'meta'])
+                ->and($props[$key])
+                ->data
+                ->toEqual($compiledResource, message: "Resource {$key} does not match");
+
+            return $this;
+        });
+
+        TestResponse::macro('assertHasResource', function (string $key, JsonResource $resource) {
+            return $this->assertInertia(fn (AssertableInertia $inertia) => $inertia->hasResource($key, $resource));
+        });
+
+        TestResponse::macro('assertHasPaginatedResource', function (string $key, ResourceCollection $resource) {
+            return $this->assertInertia(fn (AssertableInertia $inertia) => $inertia->hasPaginatedResource($key, $resource));
+        });
+    }
+}
--- tests/Feature/PatientTest.php
+++ tests/Feature/PatientTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Resources\PatientResource;
 use App\Models\Patient;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Inertia\Testing\AssertableInertia;
@@ -10,6 +11,12 @@ uses(TestCase::class, RefreshDatabase::class);
 beforeEach(function () {
     $this->user = User::factory()->create();
     $this->patient = Patient::factory()->create();
+    $this->patient_resource = PatientResource::make($this->patient);
+});
+
+it('should redirect to the login page if not authenticated', function () {
+    $response = $this->get(route('patients.index'));
+    $response->assertRedirect(route('login'));
 });
 
 it('should return the patients index component', function () {
@@ -24,20 +31,35 @@ it('load a list of patients in the index route', function () {
         ->assertInertia(fn (AssertableInertia $inertia) => $inertia->has('patients'));
 });
 
+it('load a patient in the show route', function () {
+    $this->actingAs($this->user)
+        ->get(route('patients.show', $this->patient))
+        ->assertInertia(fn (AssertableInertia $inertia) => $inertia->has('patient'));
+});
+
+it('load a patient in the edit route', function () {
+    $this->actingAs($this->user)
+        ->get(route('patients.edit', $this->patient_resource))
+        ->assertInertia(fn (AssertableInertia $inertia) => $inertia->has('patient'));
+});
+
 it('should return the patients create component', function () {
     $this->actingAs($this->user)
         ->get(route('patients.create'))
         ->assertInertia(fn (AssertableInertia $inertia) => $inertia->component('patients/create', true));
 });
 
-it('should return the patients show component', function () {
+it('should return the patients show component and load the patient info', function () {
     $this->actingAs($this->user)
-        ->get(route('patients.show'))
-        ->assertInertia(fn (AssertableInertia $inertia) => $inertia->component('patients/show', true));
+        ->get(route('patients.show', $this->patient))
+        ->assertInertia(fn (AssertableInertia $inertia) => $inertia
+            ->component('patients/show', true)
+            ->where('patient', $this->patient)
+        );
 });
 
 it('should return the patients edit component', function () {
     $this->actingAs($this->user)
-        ->get(route('patients.edit'))
+        ->get(route('patients.edit', $this->patient))
         ->assertInertia(fn (AssertableInertia $inertia) => $inertia->component('patients/edit', true));
 });
